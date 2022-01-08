08caf610750286a7439d0dbfa5f420d366b0551e
luis ricardo <58660839+lui-ricard@users.noreply.github.com>
2022-01-07 22:36:50 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var idade = 18;
+var possuiCarro = 0;
+
+if(idade >= 18){
+    console.log("Já pode tirara a ablitação");
+}
+if(idade <= 17){
+    console.log("o usuario não pode tirar a carteira");
+}
+if(possuiCarro){
+    console.log("o usuario ja pode dirigir");
+}
+
+var nome = "ricardo";
+
+if(nome == "ricardo"){
+    console.log("O seu nome é ricardo");
+}
+if(nome != "ricardo"){
+    console.log("O nome não é ricardo");
+}
